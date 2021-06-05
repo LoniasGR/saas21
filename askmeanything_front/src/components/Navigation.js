7565f08e66be4e38c764776e15a824d08750fcd0
Leonidas Avdelas <avdelasleonidas@gmail.com>
2021-06-05 12:47:38 +0300

    Testing navigation options

--- askmeanything_front/src/components/Navigation.js
+++ askmeanything_front/src/components/Navigation.js
@@ -3,15 +3,32 @@ import React from "react";
 import "../css/Navigation.css";
 
 function Navigation () {
-    return (
-        <nav>
-            <ul>
-                <li><a className={"active"} href={"/"}>Home</a></li>
-                <li className={"pos_right"}><a href={"/"}>Sign In</a></li>
-                <li className={"pos_right"}><a href={"/"}>Sign Up</a></li>
-            </ul>
-        </nav>
-    )
+
+    const username = "Leonidas";
+    const signedIn = true;
+
+    if (!signedIn) {
+        return (
+            <nav>
+                <ul>
+                    <li><a className={"active"} href={"/"}>Home</a></li>
+                    <li className={"pos_right"}><a href={"/"}>Sign In</a></li>
+                    <li className={"pos_right"}><a href={"/"}>Sign Up</a></li>
+                </ul>
+            </nav>
+        )
+    }
+    else {
+        return (
+            <nav>
+                <ul>
+                    <li><a className={"active"} href={"/"}>Home</a></li>
+                    <li className={"pos_right"}><a href={"/"}>Sign Out</a></li>
+                    <li className={"pos_right usr_welcome"}>Welcome, {`${username}`}</li>
+                </ul>
+            </nav>
+        )
+    }
 }
 
 export default Navigation;
